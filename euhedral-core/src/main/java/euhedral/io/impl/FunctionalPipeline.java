87dd4f3c6a8683236aa24574a0d3b97b66f77233
Brandon Gotay <brandon.gotay@ga-i3.com>
2026-04-21 09:47:26 -0400

    Renamed DispatchPreProcess to CacheManager
    (cherry picked from commit 55924e2261471fd7c3729f80384cb641a6afd1f9)

--- euhedral-core/src/main/java/euhedral/io/impl/FunctionalPipeline.java
+++ euhedral-core/src/main/java/euhedral/io/impl/FunctionalPipeline.java
@@ -2,13 +2,13 @@ package euhedral.io.impl;
 
 import euhedral.io.AbstractCloneablePipeline;
 import euhedral.io.control_plane.CloneConfig;
-import euhedral.io.interfaces.DispatchPreProcess;
+import euhedral.io.interfaces.CacheManager;
 import euhedral.io.interfaces.SlotManager;
 
 public class FunctionalPipeline extends AbstractCloneablePipeline {
 
     public FunctionalPipeline(String name, CloneConfig cloneConfig,
-            DispatchPreProcess preProcess,
+            CacheManager preProcess,
             SlotManager slotManager,
             FunctionalExecutor executor) {
         super(name, cloneConfig, preProcess, slotManager, executor);
@@ -16,7 +16,7 @@ public class FunctionalPipeline extends AbstractCloneablePipeline {
 
     @Override
     public FunctionalPipeline hookOnClone(CloneConfig cloneConfig) {
-        return new FunctionalPipeline(name, cloneConfig, preProcess, slotManager,
+        return new FunctionalPipeline(name, cloneConfig, cacheManager, slotManager,
                 (FunctionalExecutor) executor);
     }
 }
