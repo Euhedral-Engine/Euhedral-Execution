B-Gotech/clio-engine

Added WindowsCpuLocator as AffinityLock has not implemented it

clio-core/src/main/java/clio/io/resource_monitoring/NumaMapper.java
@@ -5,6 +5,8 @@ import java.util.BitSet;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicReferenceArray;
+
+import clio.io.resource_monitoring.providers.WindowsCpuLocator;
 import lombok.Getter;
 import net.openhft.affinity.Affinity;
 import net.openhft.affinity.AffinityLock;
@@ -19,7 +21,12 @@ public class NumaMapper {
     }
 
     public static OriginLocation locateMe() {
-        int cpu = Affinity.getCpu();
+        int cpu;
+        if(WindowsCpuLocator.LOADED) {
+            cpu = WindowsCpuLocator.INSTANCE.getCpu();
+        } else {
+            cpu = Affinity.getCpu();
+        }
         int core = INSTANCE.getPhysicalCore(cpu);
         int socket = INSTANCE.getSocket(core);
         return new OriginLocation(cpu, core, socket);

clio-core/src/main/java/clio/io/resource_monitoring/providers/WindowsCpuLocator.java
@@ -1,0 +1,42 @@
+package clio.io.resource_monitoring.providers;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WindowsCpuLocator {
+    public static final WindowsCpuLocator INSTANCE = new WindowsCpuLocator();
+
+    private WindowsCpuLocator() {
+
+    }
+
+    public static final boolean LOADED;
+    private static final Logger LOGGER = LoggerFactory.getLogger(WindowsCpuLocator.class);
+
+    static {
+        boolean loaded = false;
+        try {
+            // Test if the library can actually perform a call
+            INSTANCE.getCpu();
+            loaded = true;
+        } catch (Throwable e) {
+            LOGGER.warn("Unable to load JNA library for getting cpu", e);
+        }
+        LOADED = loaded;
+    }
+
+    public int getCpu() {
+        if(LOADED) {
+            return CLibrary.INSTANCE.GetCurrentProcessorNumber();
+        }
+        return -1;
+    }
+
+    private interface CLibrary extends Library {
+        CLibrary INSTANCE = Native.load("kernel32", CLibrary.class);
+
+        int GetCurrentProcessorNumber();
+    }
+}

Euhedral-Engine/Euhedral-Execution

euhedral-core/src/main/java/euhedral/io/resource_monitoring/NumaMapper.java
@@ -5,6 +5,8 @@ import java.util.BitSet;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicReferenceArray;
+
+import euhedral.io.resource_monitoring.providers.WindowsCpuLocator;
 import lombok.Getter;
 import net.openhft.affinity.Affinity;
 import net.openhft.affinity.AffinityLock;
@@ -19,7 +21,12 @@ public class NumaMapper {
     }
 
     public static OriginLocation locateMe() {
-        int cpu = Affinity.getCpu();
+        int cpu;
+        if(WindowsCpuLocator.LOADED) {
+            cpu = WindowsCpuLocator.INSTANCE.getCpu();
+        } else {
+            cpu = Affinity.getCpu();
+        }
         int core = INSTANCE.getPhysicalCore(cpu);
         int socket = INSTANCE.getSocket(core);
         return new OriginLocation(cpu, core, socket);

euhedral-core/src/main/java/euhedral/io/resource_monitoring/providers/WindowsCpuLocator.java
@@ -1,0 +1,42 @@
+package euhedral.io.resource_monitoring.providers;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WindowsCpuLocator {
+    public static final WindowsCpuLocator INSTANCE = new WindowsCpuLocator();
+
+    private WindowsCpuLocator() {
+
+    }
+
+    public static final boolean LOADED;
+    private static final Logger LOGGER = LoggerFactory.getLogger(WindowsCpuLocator.class);
+
+    static {
+        boolean loaded = false;
+        try {
+            // Test if the library can actually perform a call
+            INSTANCE.getCpu();
+            loaded = true;
+        } catch (Throwable e) {
+            LOGGER.warn("Unable to load JNA library for getting cpu", e);
+        }
+        LOADED = loaded;
+    }
+
+    public int getCpu() {
+        if(LOADED) {
+            return CLibrary.INSTANCE.GetCurrentProcessorNumber();
+        }
+        return -1;
+    }
+
+    private interface CLibrary extends Library {
+        CLibrary INSTANCE = Native.load("kernel32", CLibrary.class);
+
+        int GetCurrentProcessorNumber();
+    }
+}