B-Gotech/clio-engine

Moved core files to clio-core and fixed random stalls

clio-core/src/main/java/clio/io/flow_control/UpstreamQueue.java
@@ -1,0 +1,148 @@
+package clio.io.flow_control;
+
+import clio.io.utils.DrainBuffer;
+import lombok.Getter;
+import org.jctools.maps.NonBlockingHashMapLong;
+import org.jctools.queues.MpscUnboundedXaddArrayQueue;
+import org.jctools.util.PaddedAtomicLong;
+import org.reactivestreams.Subscription;
+
+public class UpstreamQueue {
+
+    public static final ThreadLocal<UpstreamQueue> UP_QUEUE = new ThreadLocal<>();
+
+    public static UpstreamQueue get(NonBlockingHashMapLong<UpstreamQueue> map,
+            PaddedAtomicLong counter) {
+        UpstreamQueue queue = UP_QUEUE.get();
+        if (queue == null) {
+            queue = new UpstreamQueue();
+            UP_QUEUE.set(queue);
+            map.put(Thread.currentThread().threadId(), queue);
+            counter.incrementAndGet();
+        }
+        return queue;
+    }
+
+    private final MpscUnboundedXaddArrayQueue<UpstreamHandle> upstreams = new MpscUnboundedXaddArrayQueue<>(
+            512);
+    private final UpstreamHandle[] drainBuffer = new UpstreamHandle[512];
+    private final int[] pullIdx = new int[]{0};
+    private final long[] pullBucket = new long[]{0, 0};
+    private final PaddedAtomicLong upstreamCount = new PaddedAtomicLong(0);
+    @Getter
+    private long cachedUpCount = 0;
+
+    public long getTrueUpstreamCount() {
+        return upstreamCount.get();
+    }
+
+    public void pull(long demand) {
+        pull(null, demand);
+    }
+
+    public void pull(DrainBuffer buffer, long demand) {
+        cachedUpCount = upstreamCount.get();
+        pullIdx[0] = 0;
+
+        if (demand == 0 || cachedUpCount == 0) {
+            return;
+        }
+
+        int count;
+        long removed = 0;
+        calculatePullBuckets(demand);
+
+        boolean workDone = true;
+        while (workDone && (count = fillUpstreamBuffer()) > 0) {
+            workDone = false;
+            for (int i = 0; i < count; i++) {
+                UpstreamHandle handle = drainBuffer[i];
+                if (!handle.isComplete()) {
+                    if (demand > 0) {
+                        long requestAmount = Math.min(demand, pullBucket[1]);
+                        demand -= requestAmount;
+                        workDone = true;
+                        drain(handle, buffer, requestAmount);
+                    }
+                    while (!upstreams.relaxedOffer(handle)) {
+                        Thread.onSpinWait();
+                    }
+                } else {
+                    drainBuffer[i] = null;
+                    removed++;
+                }
+            }
+        }
+        cachedUpCount = upstreamCount.addAndGet(-removed);
+    }
+
+    protected static void drain(UpstreamHandle handle, DrainBuffer buffer,
+            long demand) {
+        if (buffer != null) {
+            long limit = buffer.buffer.capacity() < 0 ? demand
+                    : Math.min(buffer.buffer.capacity(), demand);
+            handle.pull(buffer, limit);
+        }
+        handle.request(demand);
+    }
+
+    private void calculatePullBuckets(long demand) {
+        int start = 0;
+        int end = Math.max((int) upstreamCount.get(), 1);
+        pullBucket[0] = end - start;
+        pullBucket[1] = demand;
+
+        if (demand <= 1024 || end <= 1) {
+            pullBucket[0] = 1;
+            return;
+        }
+
+        if (demand > 32L * end) {
+            pullBucket[1] = demand / end;
+            return;
+        }
+
+        int mid = (end - start) / 2;
+
+        while (mid != 0) {
+            pullBucket[1] = demand / mid;
+            if (pullBucket[1] < 32) {
+                end = mid;
+                mid = (end - start) / 2;
+            } else {
+                mid = (end - mid) / 2;
+            }
+        }
+        pullBucket[0] = end - start;
+        pullBucket[0] = Math.max(pullBucket[0], 1);
+    }
+
+    private int fillUpstreamBuffer() {
+        if (pullBucket[0] <= 0) {
+            return 0;
+        }
+
+        pullIdx[0] = 0;
+        return upstreams.drain(
+                sub -> drainBuffer[pullIdx[0]++] = sub,
+                Math.min((int) pullBucket[0], drainBuffer.length));
+    }
+
+    public void addUpstream(UpstreamHandle upstream) {
+        while (!upstreams.relaxedOffer(upstream)) {
+            Thread.onSpinWait();
+        }
+        upstreamCount.incrementAndGet();
+    }
+
+    public static abstract class UpstreamHandle implements Subscription {
+
+        public boolean setFastPath(FluxEdge edge) {
+            return false;
+        }
+
+        public abstract void pull(DrainBuffer buffer, long demand);
+
+        public abstract boolean isComplete();
+    }
+}

clio-core/src/main/java/clio/io/utils/DrainBuffer.java
@@ -1,0 +1,50 @@
+package clio.io.utils;
+
+import clio.io.frames.AbstractFrame;
+import org.jctools.queues.MessagePassingQueue;
+
+public class DrainBuffer implements MessagePassingQueue.Consumer<AbstractFrame> {
+
+    public final MessagePassingQueue<AbstractFrame> buffer;
+
+    public final FlowRecorder arrivalLatencyRecorder = new FlowRecorder();
+
+    public long xor1 = 0;
+    public long xor2 = 0;
+    public int uniqueOrdered = 0;
+    public long drainCount = 0;
+    public long drainedBytes = 0;
+
+    public DrainBuffer(MessagePassingQueue<AbstractFrame> buffer) {
+        this.buffer = buffer;
+    }
+
+    public void reset() {
+        xor1 = 0;
+        xor2 = 0;
+        uniqueOrdered = 0;
+        drainCount = 0;
+        drainedBytes = 0;
+    }
+
+    @Override
+    public void accept(AbstractFrame frame) {
+        while (!buffer.relaxedOffer(frame)) {
+            Thread.onSpinWait();
+        }
+        if(frame.getIngestNs() > 0) {
+            long now = System.nanoTime();
+            arrivalLatencyRecorder.record(now, now - frame.getIngestNs());
+        }
+        if(frame.isOrdered()) {
+            long hash = (xor1 ^ frame.getCombinedHash()) ^ xor2;
+            if(hash != frame.getCombinedHash()) {
+                xor1 ^= frame.getCombinedHash();
+                xor2 ^= frame.getCombinedHash();
+                uniqueOrdered++;
+            }
+        }
+        drainCount++;
+        drainedBytes += frame.getSizeBytes();
+    }
+}

clio-core/src/test/java/clio/io/test_utils/TestPublisher.java
@@ -1,0 +1,62 @@
+package clio.io.test_utils;
+
+import clio.io.frames.AbstractFrame;
+import java.util.concurrent.CountDownLatch;
+import lombok.Getter;
+import org.jctools.util.PaddedAtomicLong;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class TestPublisher implements Publisher<AbstractFrame>, Subscription {
+
+    private final TestFrame[] myFrames;
+
+    private PaddedAtomicLong countDown;
+    private CountDownLatch trigger;
+    private Subscriber<? super AbstractFrame> subscriber;
+    private int internalIter = 0;
+
+    @Getter
+    private volatile boolean complete = false;
+
+    public TestPublisher(TestFrame[] frames) {
+        this.myFrames = frames;
+    }
+
+    public void reset(CountDownLatch trigger, PaddedAtomicLong countDown) {
+        this.countDown = countDown;
+        this.trigger = trigger;
+        this.internalIter = 0;
+        this.complete = false;
+    }
+
+    @Override
+    public void request(long demand) {
+        if (demand <= 0 || internalIter >= myFrames.length) {
+            return;
+        }
+
+        for (int i = 0; i < demand && internalIter < myFrames.length; i++) {
+            TestFrame f = myFrames[internalIter++];
+            f.trigger = trigger;
+            f.countDown = countDown;
+            subscriber.onNext(f);
+        }
+        if (internalIter >= myFrames.length) {
+            subscriber.onComplete();
+            complete = true;
+            subscriber = null;
+        }
+    }
+
+    @Override
+    public void cancel() {
+    }
+
+    @Override
+    public void subscribe(Subscriber<? super AbstractFrame> subscriber) {
+        this.subscriber = subscriber;
+        subscriber.onSubscribe(this);
+    }
+}

clio-core/src/test/java/clio/io/utils/ResourceMonitorTest.java
@@ -1,0 +1,99 @@
+package clio.io.utils;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import clio.io.utils.SystemUtilization.HardwareUtilization;
+import com.github.dockerjava.api.command.ExecCreateCmdResponse;
+import java.io.File;
+import java.time.Duration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.FrameConsumerResultCallback;
+import org.testcontainers.containers.output.OutputFrame.OutputType;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class ResourceMonitorTest {
+
+    private static final String RUNNER = "clio.io.utils.ResourceMonitorTest$TestRunner";
+
+    private static GenericContainer<?> container;
+
+    @BeforeAll
+    public static void buildContainer() throws Exception {
+
+        File testJar = new File("target/test-jar-with-dependencies.jar");
+
+        container = new GenericContainer<>("eclipse-temurin:21-jre-alpine")
+                .withCreateContainerCmdModifier(cmd -> cmd.getHostConfig()
+                        .withCpuQuota(150_000L) // 1.5 CPUs (Quota over 100k Period)
+                        .withCpuPeriod(100_000L)
+                        .withMemory(512 * 1024 * 1024L) // 512MB
+                        .withCpusetCpus("0,1") // Pin to physical IDs 0 and 1
+                );
+        container.addFileSystemBind(testJar.getAbsolutePath(), "/app/test.jar", BindMode.READ_ONLY);
+        container.withCommand("sleep", "3600");
+        container.start();
+    }
+
+    @Test
+    public void testDynamicScaling() throws Exception {
+        StringBuffer execOutput = new StringBuffer();
+        String containerId = container.getContainerId();
+
+        ExecCreateCmdResponse execCreateCmdResponse = container.getDockerClient()
+                .execCreateCmd(containerId)
+                .withAttachStdout(true)
+                .withAttachStderr(true)
+                .withCmd("java", "--add-exports", "java.base/jdk.internal.platform=ALL-UNNAMED",
+                        "-cp", "/app/test.jar", RUNNER)
+                .exec();
+
+        FrameConsumerResultCallback callback = new FrameConsumerResultCallback();
+        callback.addConsumer(OutputType.STDOUT, frame -> {
+            String s = frame.getUtf8String();
+            execOutput.append(s);
+        });
+        container.getDockerClient().execStartCmd(execCreateCmdResponse.getId())
+                .exec(callback);
+
+        long start = System.currentTimeMillis();
+        while (!execOutput.toString().contains("QUOTA=1.5")
+                && (System.currentTimeMillis() - start) < 2000) {
+            Thread.sleep(50);
+        }
+        assertTrue(execOutput.toString().contains("QUOTA=1.5"));
+        System.out.println("\nDetected initial quota: 1.5");
+
+        container.getDockerClient().updateContainerCmd(containerId)
+                .withCpuQuota(200_000L)
+                .withCpuPeriod(100_000L)
+                .exec();
+
+        start = System.currentTimeMillis();
+        while (!execOutput.toString().contains("QUOTA=2.0")
+                && (System.currentTimeMillis() - start) < 2000) {
+            Thread.sleep(50);
+        }
+
+        assertTrue(execOutput.toString().contains("QUOTA=2.0"));
+        System.out.println("\nDetected final quota: 2.0");
+    }
+
+    public static class TestRunner {
+
+        static void main() throws InterruptedException {
+            ResourceMonitor monitor = new ResourceMonitor(
+                    Duration.ofMillis(50));
+
+            for (int i = 0; i < 100; i++) {
+                HardwareUtilization utilization = monitor.getUtilization();
+                System.out.printf("SNAPSHOT:QUOTA=%.1f\n", utilization.quotaCpus());
+                System.out.flush();
+                Thread.sleep(200);
+            }
+        }
+    }
+}

Euhedral-Engine/Euhedral-Execution

euhedral-core/src/main/java/euhedral/io/flow_control/UpstreamQueue.java
@@ -1,0 +1,148 @@
+package euhedral.io.flow_control;
+
+import euhedral.io.utils.DrainBuffer;
+import lombok.Getter;
+import org.jctools.maps.NonBlockingHashMapLong;
+import org.jctools.queues.MpscUnboundedXaddArrayQueue;
+import org.jctools.util.PaddedAtomicLong;
+import org.reactivestreams.Subscription;
+
+public class UpstreamQueue {
+
+    public static final ThreadLocal<UpstreamQueue> UP_QUEUE = new ThreadLocal<>();
+
+    public static UpstreamQueue get(NonBlockingHashMapLong<UpstreamQueue> map,
+            PaddedAtomicLong counter) {
+        UpstreamQueue queue = UP_QUEUE.get();
+        if (queue == null) {
+            queue = new UpstreamQueue();
+            UP_QUEUE.set(queue);
+            map.put(Thread.currentThread().threadId(), queue);
+            counter.incrementAndGet();
+        }
+        return queue;
+    }
+
+    private final MpscUnboundedXaddArrayQueue<UpstreamHandle> upstreams = new MpscUnboundedXaddArrayQueue<>(
+            512);
+    private final UpstreamHandle[] drainBuffer = new UpstreamHandle[512];
+    private final int[] pullIdx = new int[]{0};
+    private final long[] pullBucket = new long[]{0, 0};
+    private final PaddedAtomicLong upstreamCount = new PaddedAtomicLong(0);
+    @Getter
+    private long cachedUpCount = 0;
+
+    public long getTrueUpstreamCount() {
+        return upstreamCount.get();
+    }
+
+    public void pull(long demand) {
+        pull(null, demand);
+    }
+
+    public void pull(DrainBuffer buffer, long demand) {
+        cachedUpCount = upstreamCount.get();
+        pullIdx[0] = 0;
+
+        if (demand == 0 || cachedUpCount == 0) {
+            return;
+        }
+
+        int count;
+        long removed = 0;
+        calculatePullBuckets(demand);
+
+        boolean workDone = true;
+        while (workDone && (count = fillUpstreamBuffer()) > 0) {
+            workDone = false;
+            for (int i = 0; i < count; i++) {
+                UpstreamHandle handle = drainBuffer[i];
+                if (!handle.isComplete()) {
+                    if (demand > 0) {
+                        long requestAmount = Math.min(demand, pullBucket[1]);
+                        demand -= requestAmount;
+                        workDone = true;
+                        drain(handle, buffer, requestAmount);
+                    }
+                    while (!upstreams.relaxedOffer(handle)) {
+                        Thread.onSpinWait();
+                    }
+                } else {
+                    drainBuffer[i] = null;
+                    removed++;
+                }
+            }
+        }
+        cachedUpCount = upstreamCount.addAndGet(-removed);
+    }
+
+    protected static void drain(UpstreamHandle handle, DrainBuffer buffer,
+            long demand) {
+        if (buffer != null) {
+            long limit = buffer.buffer.capacity() < 0 ? demand
+                    : Math.min(buffer.buffer.capacity(), demand);
+            handle.pull(buffer, limit);
+        }
+        handle.request(demand);
+    }
+
+    private void calculatePullBuckets(long demand) {
+        int start = 0;
+        int end = Math.max((int) upstreamCount.get(), 1);
+        pullBucket[0] = end - start;
+        pullBucket[1] = demand;
+
+        if (demand <= 1024 || end <= 1) {
+            pullBucket[0] = 1;
+            return;
+        }
+
+        if (demand > 32L * end) {
+            pullBucket[1] = demand / end;
+            return;
+        }
+
+        int mid = (end - start) / 2;
+
+        while (mid != 0) {
+            pullBucket[1] = demand / mid;
+            if (pullBucket[1] < 32) {
+                end = mid;
+                mid = (end - start) / 2;
+            } else {
+                mid = (end - mid) / 2;
+            }
+        }
+        pullBucket[0] = end - start;
+        pullBucket[0] = Math.max(pullBucket[0], 1);
+    }
+
+    private int fillUpstreamBuffer() {
+        if (pullBucket[0] <= 0) {
+            return 0;
+        }
+
+        pullIdx[0] = 0;
+        return upstreams.drain(
+                sub -> drainBuffer[pullIdx[0]++] = sub,
+                Math.min((int) pullBucket[0], drainBuffer.length));
+    }
+
+    public void addUpstream(UpstreamHandle upstream) {
+        while (!upstreams.relaxedOffer(upstream)) {
+            Thread.onSpinWait();
+        }
+        upstreamCount.incrementAndGet();
+    }
+
+    public static abstract class UpstreamHandle implements Subscription {
+
+        public boolean setFastPath(FluxEdge edge) {
+            return false;
+        }
+
+        public abstract void pull(DrainBuffer buffer, long demand);
+
+        public abstract boolean isComplete();
+    }
+}

euhedral-core/src/main/java/euhedral/io/utils/DrainBuffer.java
@@ -1,0 +1,50 @@
+package euhedral.io.utils;
+
+import euhedral.io.frames.AbstractFrame;
+import org.jctools.queues.MessagePassingQueue;
+
+public class DrainBuffer implements MessagePassingQueue.Consumer<AbstractFrame> {
+
+    public final MessagePassingQueue<AbstractFrame> buffer;
+
+    public final FlowRecorder arrivalLatencyRecorder = new FlowRecorder();
+
+    public long xor1 = 0;
+    public long xor2 = 0;
+    public int uniqueOrdered = 0;
+    public long drainCount = 0;
+    public long drainedBytes = 0;
+
+    public DrainBuffer(MessagePassingQueue<AbstractFrame> buffer) {
+        this.buffer = buffer;
+    }
+
+    public void reset() {
+        xor1 = 0;
+        xor2 = 0;
+        uniqueOrdered = 0;
+        drainCount = 0;
+        drainedBytes = 0;
+    }
+
+    @Override
+    public void accept(AbstractFrame frame) {
+        while (!buffer.relaxedOffer(frame)) {
+            Thread.onSpinWait();
+        }
+        if(frame.getIngestNs() > 0) {
+            long now = System.nanoTime();
+            arrivalLatencyRecorder.record(now, now - frame.getIngestNs());
+        }
+        if(frame.isOrdered()) {
+            long hash = (xor1 ^ frame.getCombinedHash()) ^ xor2;
+            if(hash != frame.getCombinedHash()) {
+                xor1 ^= frame.getCombinedHash();
+                xor2 ^= frame.getCombinedHash();
+                uniqueOrdered++;
+            }
+        }
+        drainCount++;
+        drainedBytes += frame.getSizeBytes();
+    }
+}

euhedral-core/src/test/java/euhedral/io/test_utils/TestPublisher.java
@@ -1,0 +1,62 @@
+package euhedral.io.test_utils;
+
+import euhedral.io.frames.AbstractFrame;
+import java.util.concurrent.CountDownLatch;
+import lombok.Getter;
+import org.jctools.util.PaddedAtomicLong;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class TestPublisher implements Publisher<AbstractFrame>, Subscription {
+
+    private final TestFrame[] myFrames;
+
+    private PaddedAtomicLong countDown;
+    private CountDownLatch trigger;
+    private Subscriber<? super AbstractFrame> subscriber;
+    private int internalIter = 0;
+
+    @Getter
+    private volatile boolean complete = false;
+
+    public TestPublisher(TestFrame[] frames) {
+        this.myFrames = frames;
+    }
+
+    public void reset(CountDownLatch trigger, PaddedAtomicLong countDown) {
+        this.countDown = countDown;
+        this.trigger = trigger;
+        this.internalIter = 0;
+        this.complete = false;
+    }
+
+    @Override
+    public void request(long demand) {
+        if (demand <= 0 || internalIter >= myFrames.length) {
+            return;
+        }
+
+        for (int i = 0; i < demand && internalIter < myFrames.length; i++) {
+            TestFrame f = myFrames[internalIter++];
+            f.trigger = trigger;
+            f.countDown = countDown;
+            subscriber.onNext(f);
+        }
+        if (internalIter >= myFrames.length) {
+            subscriber.onComplete();
+            complete = true;
+            subscriber = null;
+        }
+    }
+
+    @Override
+    public void cancel() {
+    }
+
+    @Override
+    public void subscribe(Subscriber<? super AbstractFrame> subscriber) {
+        this.subscriber = subscriber;
+        subscriber.onSubscribe(this);
+    }
+}

euhedral-core/src/test/java/euhedral/io/utils/ResourceMonitorTest.java
@@ -1,0 +1,99 @@
+package euhedral.io.utils;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import euhedral.io.utils.SystemUtilization.HardwareUtilization;
+import com.github.dockerjava.api.command.ExecCreateCmdResponse;
+import java.io.File;
+import java.time.Duration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.FrameConsumerResultCallback;
+import org.testcontainers.containers.output.OutputFrame.OutputType;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class ResourceMonitorTest {
+
+    private static final String RUNNER = "euhedral.io.utils.ResourceMonitorTest$TestRunner";
+
+    private static GenericContainer<?> container;
+
+    @BeforeAll
+    public static void buildContainer() throws Exception {
+
+        File testJar = new File("target/test-jar-with-dependencies.jar");
+
+        container = new GenericContainer<>("eclipse-temurin:21-jre-alpine")
+                .withCreateContainerCmdModifier(cmd -> cmd.getHostConfig()
+                        .withCpuQuota(150_000L) // 1.5 CPUs (Quota over 100k Period)
+                        .withCpuPeriod(100_000L)
+                        .withMemory(512 * 1024 * 1024L) // 512MB
+                        .withCpusetCpus("0,1") // Pin to physical IDs 0 and 1
+                );
+        container.addFileSystemBind(testJar.getAbsolutePath(), "/app/test.jar", BindMode.READ_ONLY);
+        container.withCommand("sleep", "3600");
+        container.start();
+    }
+
+    @Test
+    public void testDynamicScaling() throws Exception {
+        StringBuffer execOutput = new StringBuffer();
+        String containerId = container.getContainerId();
+
+        ExecCreateCmdResponse execCreateCmdResponse = container.getDockerClient()
+                .execCreateCmd(containerId)
+                .withAttachStdout(true)
+                .withAttachStderr(true)
+                .withCmd("java", "--add-exports", "java.base/jdk.internal.platform=ALL-UNNAMED",
+                        "-cp", "/app/test.jar", RUNNER)
+                .exec();
+
+        FrameConsumerResultCallback callback = new FrameConsumerResultCallback();
+        callback.addConsumer(OutputType.STDOUT, frame -> {
+            String s = frame.getUtf8String();
+            execOutput.append(s);
+        });
+        container.getDockerClient().execStartCmd(execCreateCmdResponse.getId())
+                .exec(callback);
+
+        long start = System.currentTimeMillis();
+        while (!execOutput.toString().contains("QUOTA=1.5")
+                && (System.currentTimeMillis() - start) < 2000) {
+            Thread.sleep(50);
+        }
+        assertTrue(execOutput.toString().contains("QUOTA=1.5"));
+        System.out.println("\nDetected initial quota: 1.5");
+
+        container.getDockerClient().updateContainerCmd(containerId)
+                .withCpuQuota(200_000L)
+                .withCpuPeriod(100_000L)
+                .exec();
+
+        start = System.currentTimeMillis();
+        while (!execOutput.toString().contains("QUOTA=2.0")
+                && (System.currentTimeMillis() - start) < 2000) {
+            Thread.sleep(50);
+        }
+
+        assertTrue(execOutput.toString().contains("QUOTA=2.0"));
+        System.out.println("\nDetected final quota: 2.0");
+    }
+
+    public static class TestRunner {
+
+        static void main() throws InterruptedException {
+            ResourceMonitor monitor = new ResourceMonitor(
+                    Duration.ofMillis(50));
+
+            for (int i = 0; i < 100; i++) {
+                HardwareUtilization utilization = monitor.getUtilization();
+                System.out.printf("SNAPSHOT:QUOTA=%.1f\n", utilization.quotaCpus());
+                System.out.flush();
+                Thread.sleep(200);
+            }
+        }
+    }
+}