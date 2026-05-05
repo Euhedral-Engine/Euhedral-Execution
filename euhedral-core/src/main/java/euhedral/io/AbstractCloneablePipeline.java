B-Gotech/clio-engine

Made it compatible with Java 17 and added support for Windows and MacOS. Also optimized more paths

clio-core/src/main/java/clio/io/AbstractCloneablePipeline.java
@@ -4,8 +4,12 @@ import clio.io.control_plane.CloneConfig;
 import clio.io.frames.AbstractFrame;
 import clio.io.interfaces.CloneableObject;
 import clio.io.interfaces.DispatchPreProcess;
+import clio.io.interfaces.PipelineExecutor;
 import clio.io.interfaces.SlotManager;
-import clio.io.utils.SystemUtilization.CoreSnapshot;
+import clio.io.resource_monitoring.SystemUtilization.CoreSnapshot;
+import clio.io.utils.PinnedThreadExecutor;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,22 +23,19 @@ public abstract class AbstractCloneablePipeline implements
 
     protected final DispatchPreProcess preProcess;
     protected final SlotManager slotManager;
-    protected final AbstractExecutor executor;
+    protected final PipelineExecutor executor;
 
     public AbstractCloneablePipeline(String name, CloneConfig cloneConfig,
             DispatchPreProcess preProcess,
             SlotManager slotManager,
-            AbstractExecutor executor) {
+            PipelineExecutor executor) {
         this.logger = LoggerFactory.getLogger(name);
         this.config = cloneConfig;
         this.name = name;
         this.preProcess = preProcess.clone(cloneConfig);
         this.slotManager = slotManager.clone(cloneConfig);
         this.executor = executor.clone(cloneConfig, slotManager.getPinnedExecutor());
-        init();
-    }
 
-    private void init() {
         preProcess.setDownstreamPressureMonitor(slotManager::getPressure);
     }
 
@@ -62,18 +63,18 @@ public abstract class AbstractCloneablePipeline implements
     }
 
     @Override
-    public Publisher<AbstractFrame> process(Publisher<AbstractFrame> frameFlux) {
+    public Publisher<? extends AbstractFrame> process(Publisher<? extends AbstractFrame> frameFlux) {
         ingest(frameFlux);
         return output();
     }
 
     @Override
-    public void ingest(Publisher<AbstractFrame> frameFlux) {
+    public void ingest(Publisher<? extends AbstractFrame> frameFlux) {
         preProcess.ingest(frameFlux);
     }
 
     @Override
-    public Publisher<AbstractFrame> output() {
+    public Publisher<? extends AbstractFrame> output() {
         return executor.output();
     }
 
@@ -115,7 +116,37 @@ public abstract class AbstractCloneablePipeline implements
     }
 
     @Override
-    public abstract AbstractCloneablePipeline clone(CloneConfig cloneConfig);
+    public final AbstractCloneablePipeline clone(CloneConfig cloneConfig) {
+        int cpu = cloneConfig.effectiveCpus().nextSetBit(0);
+
+        boolean createdExecutor = false;
+        PinnedThreadExecutor executor = PinnedThreadExecutor.get(cpu);
+
+        if(executor == null) {
+            executor = PinnedThreadExecutor.getOrSetIfAbsent(cpu, cloneConfig.shardName() + "-" + AbstractCloneablePipeline.class, Thread.MAX_PRIORITY, true);
+            createdExecutor = true;
+        }
+
+        Future<AbstractCloneablePipeline> allocated = executor.submit(() -> {
+            AbstractCloneablePipeline pipeline = hookOnClone(cloneConfig);
+            pipeline.firstTouch();
+            return pipeline;
+        });
+        AbstractCloneablePipeline retVal;
+
+        try {
+            retVal = allocated.get();
+        } catch (Throwable t) {
+            throw new RuntimeException("Failed to allocate the AbstractCloneablePipeline implementation.", t);
+        }
+
+        if(createdExecutor) {
+            executor.shutdownNow();
+        }
+        return retVal;
+    }
+
+    public abstract AbstractCloneablePipeline hookOnClone(CloneConfig cloneConfig);
 
     @Override
     public void close() throws Exception {

clio-core/src/main/java/clio/io/control_plane/CloneConfig.java
@@ -1,11 +1,18 @@
 package clio.io.control_plane;
 
-import clio.io.utils.ResourceMonitor;
+import clio.io.resource_monitoring.ResourceMonitor;
 import io.micrometer.core.instrument.MeterRegistry;
 import java.util.BitSet;
 
 public record CloneConfig(String shardName, int coreId, double quotaCpus,
                           BitSet effectiveCpus, ResourceMonitor resourceMonitor,
                           MeterRegistry meterRegistry, String metricPrefix) {
-
-};
+    public int[] getCpuSet() {
+        int[] cpus = new int[effectiveCpus.cardinality()];
+        int idx = 0;
+        for (int c = effectiveCpus.nextSetBit(0); c >= 0; c = effectiveCpus.nextSetBit(c + 1)) {
+            cpus[idx++] = c;
+        }
+        return cpus;
+    }
+}

clio-core/src/main/java/clio/io/frames/AbstractFrame.java
@@ -1,12 +1,12 @@
 package clio.io.frames;
 
+import clio.io.control_plane.RoutingPolicy;
 import clio.io.errors.WorkCancelled;
-import clio.io.utils.KeyHasher;
+import clio.io.flow_control.LockFreeSink;
+import clio.io.resource_monitoring.NumaMapper.OriginLocation;
 import clio.io.utils.MpscFrameRecycler;
 import lombok.Getter;
 import lombok.Setter;
-import reactor.core.publisher.Sinks.EmitResult;
-import reactor.core.publisher.Sinks.Many;
 
 /**
  * Base unit of work within the Clio Execution Fabric.
@@ -19,8 +19,8 @@ import reactor.core.publisher.Sinks.Many;
  * to be reset and dispatched again.
  *
  * <p><b>Ordering:</b> Reliable sequencing depends on unique, well-distributed hashes.
- * Use {@link clio.io.utils.KeyHasher} to prevent collisions that can
- * degrade performance or break ordering guarantees.
+ * Use {@link clio.io.utils.KeyHasher} to prevent collisions that can degrade performance or break
+ * ordering guarantees.
  */
 public abstract class AbstractFrame extends WorkCancelled {
 
@@ -31,8 +31,10 @@ public abstract class AbstractFrame extends WorkCancelled {
     protected volatile long combinedHash;
     @Getter
     @Setter
-    private long destinationHash;
-
+    private OriginLocation origin;
+    @Getter
+    @Setter
+    private RoutingPolicy routingPolicy;
     @Getter
     @Setter
     private long startNs;
@@ -41,7 +43,7 @@ public abstract class AbstractFrame extends WorkCancelled {
     @Setter
     private long ingestNs;
 
-    private Many<AbstractFrame> completionSink;
+    private LockFreeSink completionSink;
     private long notifyCompletePassword;
 
     @Getter
@@ -56,15 +58,11 @@ public abstract class AbstractFrame extends WorkCancelled {
     @Setter
     private boolean useVThread = false;
 
-    public AbstractFrame(long idHash, long destinationHash,
-            MpscFrameRecycler recycler) {
+    public AbstractFrame(long idHash, MpscFrameRecycler recycler) {
         this.idHash = idHash;
-        this.destinationHash = destinationHash;
         this.recycler = recycler;
-        this.combinedHash = KeyHasher.combine(idHash, destinationHash);
+        this.combinedHash = idHash;
     }
-
-    public abstract String getId();
 
     public final long getIdHash() {
         return idHash;
@@ -93,7 +91,7 @@ public abstract class AbstractFrame extends WorkCancelled {
      *
      * @param completionSink Sink to return the frame to.
      */
-    public final void setCompletionSink(Many<AbstractFrame> completionSink) {
+    public final void setCompletionSink(LockFreeSink completionSink) {
         this.completionSink = completionSink;
     }
 
@@ -114,17 +112,9 @@ public abstract class AbstractFrame extends WorkCancelled {
      *
      * @param password notifyCompletePassword password
      */
-    public final void notifyComplete(long startNs, long password) {
-        this.startNs = startNs;
+    public final void notifyComplete(long password) {
         if (notifyCompletePassword == password) {
-            EmitResult result;
-            while (!(result = completionSink.tryEmitNext(this)).isSuccess()) {
-                if (result == EmitResult.FAIL_CANCELLED || result == EmitResult.FAIL_TERMINATED
-                        || result == EmitResult.FAIL_ZERO_SUBSCRIBER) {
-                    this.kill();
-                    throw new IllegalStateException(
-                            "CRITICAL: No upstream connection to signal cancellation.");
-                }
+            while (!completionSink.relaxedOffer(this)) {
                 Thread.onSpinWait();
             }
             notifyCompletePassword = 0;
@@ -143,21 +133,32 @@ public abstract class AbstractFrame extends WorkCancelled {
      *
      */
     public void doFinally() {
-        setCancelledExecution(false);
+        reset();
         recycle();
+    }
+
+    public final void reset() {
+        combinedHash = idHash;
+        startNs = 0;
+        ingestNs = 0;
+        completionSink = null;
+        notifyCompletePassword = 0;
+        cancelledExecution = false;
     }
 
     /**
      * Sends the frame back to the creator for reuse.
      */
     public final void recycle() {
-        recycler.recycle(this);
+        if (recycler != null) {
+            recycler.recycle(this);
+        }
     }
 
     /**
      * Throws this class as an error. This is used as a way to quickly stop execution of this frame.
-     * {@link com.ccri.clio.common.io.dispatch AbstractExecutor} handles this by default. UpstreamHandle
-     * callers should handle this error.
+     * {@link com.ccri.clio.common.io.dispatch AbstractExecutor} handles this by default.
+     * UpstreamHandle callers should handle this error.
      */
     public final void throwMeAsError() {
         throw this;

clio-core/src/main/java/clio/io/impl/FunctionalPipeline.java
@@ -1,0 +1,22 @@
+package clio.io.impl;
+
+import clio.io.AbstractCloneablePipeline;
+import clio.io.control_plane.CloneConfig;
+import clio.io.interfaces.DispatchPreProcess;
+import clio.io.interfaces.SlotManager;
+
+public class FunctionalPipeline extends AbstractCloneablePipeline {
+
+    public FunctionalPipeline(String name, CloneConfig cloneConfig,
+            DispatchPreProcess preProcess,
+            SlotManager slotManager,
+            FunctionalExecutor executor) {
+        super(name, cloneConfig, preProcess, slotManager, executor);
+    }
+
+    @Override
+    public FunctionalPipeline hookOnClone(CloneConfig cloneConfig) {
+        return new FunctionalPipeline(name, cloneConfig, preProcess, slotManager,
+                (FunctionalExecutor) executor);
+    }
+}

clio-core/src/main/resources/monitoring/mac_resources.cpp
@@ -1,0 +1,107 @@
+#include <jni.h>
+
+#ifdef __APPLE__
+
+#include <libproc.h>
+#include <mach/mach.h>
+#include <mach/mach_host.h>
+#include <mach/task_info.h>
+#include <sys/resource.h>
+#include <sys/sysctl.h>
+#include <unistd.h>
+
+extern "C" {
+
+// CPU Times
+JNIEXPORT jlongArray JNICALL
+Java_clio_io_resource_1monitoring_providers_MacOSResources_getCpuTimes(JNIEnv* env, jclass) {
+
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+
+    jlong user = (jlong)usage.ru_utime.tv_sec * 1'000'000'000LL +
+                 (jlong)usage.ru_utime.tv_usec * 1000;
+
+    jlong sys = (jlong)usage.ru_stime.tv_sec * 1'000'000'000LL +
+                (jlong)usage.ru_stime.tv_usec * 1000;
+
+    jlong total = user + sys;
+
+    jlongArray result = env->NewLongArray(2);
+    jlong values[2] = { total, 0 };
+
+    env->SetLongArrayRegion(result, 0, 2, values);
+    return result;
+}
+
+// CPU Load (system-wide)
+JNIEXPORT jdouble JNICALL
+Java_clio_io_resource_1monitoring_providers_MacOSResources_getSystemCpuLoad(JNIEnv*, jclass) {
+
+    host_cpu_load_info_data_t cpuinfo;
+    mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+
+    if (host_statistics(mach_host_self(),
+                        HOST_CPU_LOAD_INFO,
+                        (host_info_t)&cpuinfo,
+                        &count) != KERN_SUCCESS) {
+        return 0.0;
+    }
+
+    double user = cpuinfo.cpu_ticks[CPU_STATE_USER];
+    double system = cpuinfo.cpu_ticks[CPU_STATE_SYSTEM];
+    double idle = cpuinfo.cpu_ticks[CPU_STATE_IDLE];
+    double nice = cpuinfo.cpu_ticks[CPU_STATE_NICE];
+
+    double total = user + system + idle + nice;
+
+    return total > 0 ? (user + system) / total : 0.0;
+}
+
+
+// Memory
+JNIEXPORT jlongArray JNICALL
+Java_clio_io_resource_1monitoring_providers_MacOSResources_getMemorySnapshot(JNIEnv* env, jclass) {
+
+    jlong values[3] = {0, 0, 0};
+
+    // Total system memory
+    int64_t mem;
+    size_t len = sizeof(mem);
+    sysctlbyname("hw.memsize", &mem, &len, NULL, 0);
+
+    values[0] = (jlong)mem;
+
+    // Process memory
+    mach_task_basic_info info;
+    mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+
+    if (task_info(mach_task_self(),
+                  MACH_TASK_BASIC_INFO,
+                  (task_info_t)&info,
+                  &count) == KERN_SUCCESS) {
+
+        values[1] = (jlong)info.resident_size;
+        values[2] = (jlong)(info.virtual_size - info.resident_size);
+    }
+
+    jlongArray result = env->NewLongArray(3);
+    env->SetLongArrayRegion(result, 0, 3, values);
+
+    return result;
+}
+
+
+// IO Bytes
+JNIEXPORT jlong JNICALL
+Java_clio_io_resource_1monitoring_providers_MacOSResources_getIoBytes(JNIEnv*, jclass) {
+    struct rusage_info_v3 rusage;
+    if (proc_pid_rusage(getpid(), RUSAGE_INFO_V3, (void **)&rusage) == 0) {
+        return (jlong)(rusage.ri_diskio_bytesread + rusage.ri_diskio_byteswritten);
+    }
+    return 0;
+}
+
+}
+
+#endif

Euhedral-Engine/Euhedral-Execution

euhedral-core/src/main/java/euhedral/io/AbstractCloneablePipeline.java
@@ -4,8 +4,12 @@ import euhedral.io.control_plane.CloneConfig;
 import euhedral.io.frames.AbstractFrame;
 import euhedral.io.interfaces.CloneableObject;
 import euhedral.io.interfaces.DispatchPreProcess;
+import euhedral.io.interfaces.PipelineExecutor;
 import euhedral.io.interfaces.SlotManager;
-import euhedral.io.utils.SystemUtilization.CoreSnapshot;
+import euhedral.io.resource_monitoring.SystemUtilization.CoreSnapshot;
+import euhedral.io.utils.PinnedThreadExecutor;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,22 +23,19 @@ public abstract class AbstractCloneablePipeline implements
 
     protected final DispatchPreProcess preProcess;
     protected final SlotManager slotManager;
-    protected final AbstractExecutor executor;
+    protected final PipelineExecutor executor;
 
     public AbstractCloneablePipeline(String name, CloneConfig cloneConfig,
             DispatchPreProcess preProcess,
             SlotManager slotManager,
-            AbstractExecutor executor) {
+            PipelineExecutor executor) {
         this.logger = LoggerFactory.getLogger(name);
         this.config = cloneConfig;
         this.name = name;
         this.preProcess = preProcess.clone(cloneConfig);
         this.slotManager = slotManager.clone(cloneConfig);
         this.executor = executor.clone(cloneConfig, slotManager.getPinnedExecutor());
-        init();
-    }
 
-    private void init() {
         preProcess.setDownstreamPressureMonitor(slotManager::getPressure);
     }
 
@@ -62,18 +63,18 @@ public abstract class AbstractCloneablePipeline implements
     }
 
     @Override
-    public Publisher<AbstractFrame> process(Publisher<AbstractFrame> frameFlux) {
+    public Publisher<? extends AbstractFrame> process(Publisher<? extends AbstractFrame> frameFlux) {
         ingest(frameFlux);
         return output();
     }
 
     @Override
-    public void ingest(Publisher<AbstractFrame> frameFlux) {
+    public void ingest(Publisher<? extends AbstractFrame> frameFlux) {
         preProcess.ingest(frameFlux);
     }
 
     @Override
-    public Publisher<AbstractFrame> output() {
+    public Publisher<? extends AbstractFrame> output() {
         return executor.output();
     }
 
@@ -115,7 +116,37 @@ public abstract class AbstractCloneablePipeline implements
     }
 
     @Override
-    public abstract AbstractCloneablePipeline clone(CloneConfig cloneConfig);
+    public final AbstractCloneablePipeline clone(CloneConfig cloneConfig) {
+        int cpu = cloneConfig.effectiveCpus().nextSetBit(0);
+
+        boolean createdExecutor = false;
+        PinnedThreadExecutor executor = PinnedThreadExecutor.get(cpu);
+
+        if(executor == null) {
+            executor = PinnedThreadExecutor.getOrSetIfAbsent(cpu, cloneConfig.shardName() + "-" + AbstractCloneablePipeline.class, Thread.MAX_PRIORITY, true);
+            createdExecutor = true;
+        }
+
+        Future<AbstractCloneablePipeline> allocated = executor.submit(() -> {
+            AbstractCloneablePipeline pipeline = hookOnClone(cloneConfig);
+            pipeline.firstTouch();
+            return pipeline;
+        });
+        AbstractCloneablePipeline retVal;
+
+        try {
+            retVal = allocated.get();
+        } catch (Throwable t) {
+            throw new RuntimeException("Failed to allocate the AbstractCloneablePipeline implementation.", t);
+        }
+
+        if(createdExecutor) {
+            executor.shutdownNow();
+        }
+        return retVal;
+    }
+
+    public abstract AbstractCloneablePipeline hookOnClone(CloneConfig cloneConfig);
 
     @Override
     public void close() throws Exception {

euhedral-core/src/main/java/euhedral/io/control_plane/CloneConfig.java
@@ -1,11 +1,18 @@
 package euhedral.io.control_plane;
 
-import euhedral.io.utils.ResourceMonitor;
+import euhedral.io.resource_monitoring.ResourceMonitor;
 import io.micrometer.core.instrument.MeterRegistry;
 import java.util.BitSet;
 
 public record CloneConfig(String shardName, int coreId, double quotaCpus,
                           BitSet effectiveCpus, ResourceMonitor resourceMonitor,
                           MeterRegistry meterRegistry, String metricPrefix) {
-
-};
+    public int[] getCpuSet() {
+        int[] cpus = new int[effectiveCpus.cardinality()];
+        int idx = 0;
+        for (int c = effectiveCpus.nextSetBit(0); c >= 0; c = effectiveCpus.nextSetBit(c + 1)) {
+            cpus[idx++] = c;
+        }
+        return cpus;
+    }
+}

euhedral-core/src/main/java/euhedral/io/frames/AbstractFrame.java
@@ -1,12 +1,12 @@
 package euhedral.io.frames;
 
+import euhedral.io.control_plane.RoutingPolicy;
 import euhedral.io.errors.WorkCancelled;
-import euhedral.io.utils.KeyHasher;
+import euhedral.io.flow_control.LockFreeSink;
+import euhedral.io.resource_monitoring.NumaMapper.OriginLocation;
 import euhedral.io.utils.MpscFrameRecycler;
 import lombok.Getter;
 import lombok.Setter;
-import reactor.core.publisher.Sinks.EmitResult;
-import reactor.core.publisher.Sinks.Many;
 
 /**
  * Base unit of work within the Clio Execution Fabric.
@@ -19,8 +19,8 @@ import reactor.core.publisher.Sinks.Many;
  * to be reset and dispatched again.
  *
  * <p><b>Ordering:</b> Reliable sequencing depends on unique, well-distributed hashes.
- * Use {@link euhedral.io.utils.KeyHasher} to prevent collisions that can
- * degrade performance or break ordering guarantees.
+ * Use {@link euhedral.io.utils.KeyHasher} to prevent collisions that can degrade performance or break
+ * ordering guarantees.
  */
 public abstract class AbstractFrame extends WorkCancelled {
 
@@ -31,8 +31,10 @@ public abstract class AbstractFrame extends WorkCancelled {
     protected volatile long combinedHash;
     @Getter
     @Setter
-    private long destinationHash;
-
+    private OriginLocation origin;
+    @Getter
+    @Setter
+    private RoutingPolicy routingPolicy;
     @Getter
     @Setter
     private long startNs;
@@ -41,7 +43,7 @@ public abstract class AbstractFrame extends WorkCancelled {
     @Setter
     private long ingestNs;
 
-    private Many<AbstractFrame> completionSink;
+    private LockFreeSink completionSink;
     private long notifyCompletePassword;
 
     @Getter
@@ -56,15 +58,11 @@ public abstract class AbstractFrame extends WorkCancelled {
     @Setter
     private boolean useVThread = false;
 
-    public AbstractFrame(long idHash, long destinationHash,
-            MpscFrameRecycler recycler) {
+    public AbstractFrame(long idHash, MpscFrameRecycler recycler) {
         this.idHash = idHash;
-        this.destinationHash = destinationHash;
         this.recycler = recycler;
-        this.combinedHash = KeyHasher.combine(idHash, destinationHash);
+        this.combinedHash = idHash;
     }
-
-    public abstract String getId();
 
     public final long getIdHash() {
         return idHash;
@@ -93,7 +91,7 @@ public abstract class AbstractFrame extends WorkCancelled {
      *
      * @param completionSink Sink to return the frame to.
      */
-    public final void setCompletionSink(Many<AbstractFrame> completionSink) {
+    public final void setCompletionSink(LockFreeSink completionSink) {
         this.completionSink = completionSink;
     }
 
@@ -114,17 +112,9 @@ public abstract class AbstractFrame extends WorkCancelled {
      *
      * @param password notifyCompletePassword password
      */
-    public final void notifyComplete(long startNs, long password) {
-        this.startNs = startNs;
+    public final void notifyComplete(long password) {
         if (notifyCompletePassword == password) {
-            EmitResult result;
-            while (!(result = completionSink.tryEmitNext(this)).isSuccess()) {
-                if (result == EmitResult.FAIL_CANCELLED || result == EmitResult.FAIL_TERMINATED
-                        || result == EmitResult.FAIL_ZERO_SUBSCRIBER) {
-                    this.kill();
-                    throw new IllegalStateException(
-                            "CRITICAL: No upstream connection to signal cancellation.");
-                }
+            while (!completionSink.relaxedOffer(this)) {
                 Thread.onSpinWait();
             }
             notifyCompletePassword = 0;
@@ -143,21 +133,32 @@ public abstract class AbstractFrame extends WorkCancelled {
      *
      */
     public void doFinally() {
-        setCancelledExecution(false);
+        reset();
         recycle();
+    }
+
+    public final void reset() {
+        combinedHash = idHash;
+        startNs = 0;
+        ingestNs = 0;
+        completionSink = null;
+        notifyCompletePassword = 0;
+        cancelledExecution = false;
     }
 
     /**
      * Sends the frame back to the creator for reuse.
      */
     public final void recycle() {
-        recycler.recycle(this);
+        if (recycler != null) {
+            recycler.recycle(this);
+        }
     }
 
     /**
      * Throws this class as an error. This is used as a way to quickly stop execution of this frame.
-     * {@link euhedral.common.io.dispatch AbstractExecutor} handles this by default. UpstreamHandle
-     * callers should handle this error.
+     * {@link com.ccri.euhedral.common.io.dispatch AbstractExecutor} handles this by default.
+     * UpstreamHandle callers should handle this error.
      */
     public final void throwMeAsError() {
         throw this;

euhedral-core/src/main/java/euhedral/io/impl/FunctionalPipeline.java
@@ -1,0 +1,22 @@
+package euhedral.io.impl;
+
+import euhedral.io.AbstractCloneablePipeline;
+import euhedral.io.control_plane.CloneConfig;
+import euhedral.io.interfaces.DispatchPreProcess;
+import euhedral.io.interfaces.SlotManager;
+
+public class FunctionalPipeline extends AbstractCloneablePipeline {
+
+    public FunctionalPipeline(String name, CloneConfig cloneConfig,
+            DispatchPreProcess preProcess,
+            SlotManager slotManager,
+            FunctionalExecutor executor) {
+        super(name, cloneConfig, preProcess, slotManager, executor);
+    }
+
+    @Override
+    public FunctionalPipeline hookOnClone(CloneConfig cloneConfig) {
+        return new FunctionalPipeline(name, cloneConfig, preProcess, slotManager,
+                (FunctionalExecutor) executor);
+    }
+}

euhedral-core/src/main/resources/monitoring/mac_resources.cpp
@@ -1,0 +1,107 @@
+#include <jni.h>
+
+#ifdef __APPLE__
+
+#include <libproc.h>
+#include <mach/mach.h>
+#include <mach/mach_host.h>
+#include <mach/task_info.h>
+#include <sys/resource.h>
+#include <sys/sysctl.h>
+#include <unistd.h>
+
+extern "C" {
+
+// CPU Times
+JNIEXPORT jlongArray JNICALL
+Java_euhedral_io_resource_1monitoring_providers_MacOSResources_getCpuTimes(JNIEnv* env, jclass) {
+
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+
+    jlong user = (jlong)usage.ru_utime.tv_sec * 1'000'000'000LL +
+                 (jlong)usage.ru_utime.tv_usec * 1000;
+
+    jlong sys = (jlong)usage.ru_stime.tv_sec * 1'000'000'000LL +
+                (jlong)usage.ru_stime.tv_usec * 1000;
+
+    jlong total = user + sys;
+
+    jlongArray result = env->NewLongArray(2);
+    jlong values[2] = { total, 0 };
+
+    env->SetLongArrayRegion(result, 0, 2, values);
+    return result;
+}
+
+// CPU Load (system-wide)
+JNIEXPORT jdouble JNICALL
+Java_euhedral_io_resource_1monitoring_providers_MacOSResources_getSystemCpuLoad(JNIEnv*, jclass) {
+
+    host_cpu_load_info_data_t cpuinfo;
+    mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+
+    if (host_statistics(mach_host_self(),
+                        HOST_CPU_LOAD_INFO,
+                        (host_info_t)&cpuinfo,
+                        &count) != KERN_SUCCESS) {
+        return 0.0;
+    }
+
+    double user = cpuinfo.cpu_ticks[CPU_STATE_USER];
+    double system = cpuinfo.cpu_ticks[CPU_STATE_SYSTEM];
+    double idle = cpuinfo.cpu_ticks[CPU_STATE_IDLE];
+    double nice = cpuinfo.cpu_ticks[CPU_STATE_NICE];
+
+    double total = user + system + idle + nice;
+
+    return total > 0 ? (user + system) / total : 0.0;
+}
+
+
+// Memory
+JNIEXPORT jlongArray JNICALL
+Java_euhedral_io_resource_1monitoring_providers_MacOSResources_getMemorySnapshot(JNIEnv* env, jclass) {
+
+    jlong values[3] = {0, 0, 0};
+
+    // Total system memory
+    int64_t mem;
+    size_t len = sizeof(mem);
+    sysctlbyname("hw.memsize", &mem, &len, NULL, 0);
+
+    values[0] = (jlong)mem;
+
+    // Process memory
+    mach_task_basic_info info;
+    mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+
+    if (task_info(mach_task_self(),
+                  MACH_TASK_BASIC_INFO,
+                  (task_info_t)&info,
+                  &count) == KERN_SUCCESS) {
+
+        values[1] = (jlong)info.resident_size;
+        values[2] = (jlong)(info.virtual_size - info.resident_size);
+    }
+
+    jlongArray result = env->NewLongArray(3);
+    env->SetLongArrayRegion(result, 0, 3, values);
+
+    return result;
+}
+
+
+// IO Bytes
+JNIEXPORT jlong JNICALL
+Java_euhedral_io_resource_1monitoring_providers_MacOSResources_getIoBytes(JNIEnv*, jclass) {
+    struct rusage_info_v3 rusage;
+    if (proc_pid_rusage(getpid(), RUSAGE_INFO_V3, (void **)&rusage) == 0) {
+        return (jlong)(rusage.ri_diskio_bytesread + rusage.ri_diskio_byteswritten);
+    }
+    return 0;
+}
+
+}
+
+#endif