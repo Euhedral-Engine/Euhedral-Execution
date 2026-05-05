B-Gotech/clio-engine

Added some utility classes and moved files

clio-core/src/main/java/clio/io/frames/ConsumerFrame.java
@@ -1,6 +1,6 @@
 package clio.io.frames;
 
-import clio.io.utils.MpscFrameRecycler;
+import clio.io.impl.FrameManager;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import lombok.Setter;
@@ -13,7 +13,7 @@ public class ConsumerFrame extends AbstractFrame {
     private Object payload;
 
     public ConsumerFrame(long idHash, Consumer<Object> consumer, AtomicBoolean killSwitch,
-            MpscFrameRecycler recycler) {
+            FrameManager<Object, ConsumerFrame> recycler) {
         super(idHash, recycler);
         this.consumer = consumer;
         this.killSwitch = killSwitch;

clio-core/src/main/java/clio/io/frames/FunctionFrame.java
@@ -1,6 +1,6 @@
 package clio.io.frames;
 
-import clio.io.utils.MpscFrameRecycler;
+import clio.io.impl.FrameManager;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -18,7 +18,7 @@ public class FunctionFrame extends AbstractFrame {
 
 
     public FunctionFrame(long idHash, Function<Object, Object> function, Consumer<Object> callback, AtomicBoolean killSwitch,
-            MpscFrameRecycler recycler) {
+            FrameManager<Object, FunctionFrame> recycler) {
         super(idHash, recycler);
         this.function = function;
         this.callback = callback;

clio-core/src/main/java/clio/io/frames/SequencedFrame.java
@@ -1,6 +1,7 @@
 package clio.io.frames;
 
-import clio.io.utils.MpscFrameRecycler;
+import clio.io.impl.FrameSequencer;
+import clio.io.impl.FrameManager;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import lombok.Getter;
@@ -34,7 +35,7 @@ public class SequencedFrame extends AbstractFrame {
 
     public SequencedFrame(long idHash, int sequenceNumber,
             Object payload, Function<Object, Object> function, AtomicBoolean killSwitch, FrameSequencer<Object> sequencer,
-            MpscFrameRecycler recycler) {
+            FrameManager<Object, SequencedFrame> recycler) {
         super(idHash, recycler);
         this.killSwitch = killSwitch;
         this.sequencer = sequencer;

clio-core/src/main/java/clio/io/impl/FrameFactory.java
@@ -1,0 +1,50 @@
+package clio.io.impl;
+
+import clio.io.frames.AbstractFrame;
+import clio.io.utils.KeyHasher;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class FrameFactory<T, F extends AbstractFrame> {
+    private final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
+
+    private final FrameCreate<T, F> frameGenerator;
+    private final FrameReplace<T, F> frameReplace;
+
+    private long seed = ThreadLocalRandom.current().nextLong();
+
+    public FrameFactory(FrameCreate<T, F> frameGenerator, FrameReplace<T, F> frameReplace) {
+        this.frameGenerator = frameGenerator;
+        this.frameReplace = frameReplace;
+    }
+
+    public F create(T data) {
+        F frame = frameGenerator.create(idHash, data);
+        if(frame.isOrdered()) {
+            frame.randomizeHash(idHash);
+        } else {
+            frame.randomizeHash(seed++);
+        }
+        return frame;
+    }
+
+    public void replace(T data, F frame) {
+        frame.reset();
+
+        frameReplace.replace(data, frame);
+        if(frame.isOrdered()) {
+            frame.randomizeHash(idHash);
+        } else {
+            frame.randomizeHash(seed++);
+        }
+    }
+
+    @FunctionalInterface
+    public interface FrameCreate<T, F extends AbstractFrame> {
+        F create(long idHash, T data);
+    }
+
+    @FunctionalInterface
+    public interface FrameReplace<T, F extends AbstractFrame> {
+        void replace(T data, F oldFrame);
+    }
+}

clio-core/src/main/java/clio/io/impl/FrameSequencer.java
@@ -1,7 +1,7 @@
-package clio.io.frames;
+package clio.io.impl;
 
+import clio.io.frames.SequencedFrame;
 import clio.io.utils.KeyHasher;
-import clio.io.utils.MpscFrameRecycler;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.longs.LongHeapPriorityQueue;
 import java.util.concurrent.ThreadLocalRandom;
@@ -15,7 +15,7 @@ import reactor.core.scheduler.Schedulers;
 
 public class FrameSequencer<R> {
 
-    private static final SequencedFrame SIGNAL_FRAME = new SequencedFrame( -1, -1, null, null,
+    private static final SequencedFrame SIGNAL_FRAME = new SequencedFrame(-1, -1, null, null,
             null, null, null);
 
     private final long ingestPassword;
@@ -36,68 +36,12 @@ public class FrameSequencer<R> {
         Flux.merge(ingest.asFlux())
                 .publishOn(Schedulers.boundedElastic())
                 .subscribe(frame -> {
-                    if(frame.getSequenceNumber() != -1) {
+                    if (frame.getSequenceNumber() != -1) {
                         frameData.put(frame.getSequenceNumber(), frame);
                         sequenceHeap.enqueue(frame.getSequenceNumber());
                     }
                     drainInternal();
                 });
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> Flux<AbstractFrame> map(Flux<T> flux, Function<T, R> function) {
-        final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
-        final AtomicBoolean killSwitch = new AtomicBoolean(false);
-
-        final long[] seed = new long[]{KeyHasher.combine(ThreadLocalRandom.current().nextLong(), ThreadLocalRandom.current().nextLong())};
-
-        final MpscFrameRecycler recycler = new MpscFrameRecycler(32_768, ingestPassword);
-
-        final int[] idx = new int[]{-1, 0};
-        final SequencedFrame[] buffer = new SequencedFrame[1024];
-
-        return flux.map(obj -> {
-            if (idx[0] < 0) {
-                idx[0] = recycler.batchPoll(buffer, ingestPassword);
-            }
-
-            SequencedFrame sequencedFrame;
-            if (--idx[0] >= 0) {
-                sequencedFrame = buffer[idx[0]];
-                sequencedFrame.replace(idx[1]++, obj);
-            } else {
-                sequencedFrame = new SequencedFrame(idHash, idx[1]++, obj,
-                        (Function<Object, Object>) function,
-                        killSwitch, (FrameSequencer<Object>) this, recycler);
-            }
-            sequencedFrame.setOrdered(false);
-            sequencedFrame.randomizeHash(seed[0]++);
-            registerFrame(sequencedFrame);
-
-            return (AbstractFrame) sequencedFrame;
-        }).doFinally(sig -> {
-            killSwitch.set(true);
-            sequenceHeap.clear();
-            frameData.clear();
-            ingest.tryEmitComplete();
-            output.tryEmitComplete();
-            recycler.close();
-        });
-    }
-
-    public Flux<R> output() {
-        return output.asFlux();
-    }
-
-    public void notifyComplete(long password) {
-        if(password == sequencePassword) {
-            ingest.tryEmitNext(SIGNAL_FRAME);
-        }
-    }
-
-    private void registerFrame(SequencedFrame frame) {
-        frame.setSequencerPassword(sequencePassword);
-        ingest.tryEmitNext(frame);
     }
 
     @SuppressWarnings("unchecked")
@@ -127,6 +71,52 @@ public class FrameSequencer<R> {
             } else {
                 break;
             }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> Flux<SequencedFrame> map(Flux<T> flux, Function<T, R> function) {
+        final AtomicBoolean killSwitch = new AtomicBoolean(false);
+
+        final int[] idx = new int[]{0};
+
+        FrameManager<Object, SequencedFrame> recycler = new FrameManager<>(32_768, ingestPassword);
+        recycler.setFactory(new FrameFactory<>((idHash, data) -> {
+            SequencedFrame frame = new SequencedFrame(idHash, idx[0]++, data,
+                    (Function<Object, Object>) function,
+                    killSwitch, (FrameSequencer<Object>) this, recycler);
+            frame.setOrdered(false);
+            registerFrame(frame);
+            return frame;
+        }, (data, oldFrame) -> {
+            oldFrame.replace(idx[0]++, data);
+            oldFrame.setOrdered(false);
+            registerFrame(oldFrame);
+        }));
+
+        return flux.map(obj -> recycler.generate(obj, ingestPassword))
+                .doFinally(sig -> {
+                    killSwitch.set(true);
+                    sequenceHeap.clear();
+                    frameData.clear();
+                    ingest.tryEmitComplete();
+                    output.tryEmitComplete();
+                    recycler.close();
+                });
+    }
+
+    private void registerFrame(SequencedFrame frame) {
+        frame.setSequencerPassword(sequencePassword);
+        ingest.tryEmitNext(frame);
+    }
+
+    public Flux<R> output() {
+        return output.asFlux();
+    }
+
+    public void notifyComplete(long password) {
+        if (password == sequencePassword) {
+            ingest.tryEmitNext(SIGNAL_FRAME);
         }
     }
 }

clio-core/src/test/java/clio/io/test_utils/TestFrame.java
@@ -4,48 +4,57 @@ import clio.io.control_plane.RoutingPolicy;
 import clio.io.frames.AbstractFrame;
 import clio.io.resource_monitoring.NumaMapper;
 import clio.io.utils.KeyHasher;
-import java.util.concurrent.CountDownLatch;
+import clio.io.impl.FrameManager;
 import java.util.concurrent.ThreadLocalRandom;
 import org.jctools.util.PaddedAtomicLong;
 
 public final class TestFrame extends AbstractFrame {
+    public static final long PASSWORD = 123;
 
     public long startNs;
     public PaddedAtomicLong countDown;
-    public CountDownLatch trigger;
     public boolean ordered;
 
-    public TestFrame(long idHash) {
-        this(idHash, true);
+    public TestFrame(long idHash, FrameManager<Void, TestFrame> recycler) {
+        this(idHash, true, recycler);
     }
 
-    public TestFrame(long idHash, boolean ordered) {
-        super(idHash, null);
+    public TestFrame(long idHash, boolean ordered, FrameManager<Void, TestFrame> recycler) {
+        super(idHash, recycler);
         this.ordered = ordered;
         this.setOrigin(NumaMapper.locateMe());
     }
 
     public static TestFrame[] generateParallel(int count) {
+        FrameManager<Void, TestFrame> recycler = new FrameManager<>(count, PASSWORD);
+
         final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
         TestFrame[] frames = new TestFrame[count];
         for (int i = 0; i < count; i++) {
-            frames[i] = new TestFrame(idHash, false);
+            frames[i] = new TestFrame(idHash, false, recycler);
             frames[i].randomizeHash(KeyHasher.getHash("seed-" + i));
         }
         return frames;
     }
 
     public static TestFrame[] generateOrdered(int count) {
+        FrameManager<Void, TestFrame> recycler = new FrameManager<>(count, PASSWORD);
+
         final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
         final long orderedSeed = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
 
         TestFrame[] frames = new TestFrame[count];
         for(int i = 0; i < count; i++) {
-            frames[i] = new TestFrame(idHash, true);
+            frames[i] = new TestFrame(idHash, true, recycler);
             frames[i].randomizeHash(orderedSeed);
             frames[i].setRoutingPolicy(RoutingPolicy.CORE_LOCAL);
         }
         return frames;
+    }
+
+    @SuppressWarnings("unchecked")
+    public FrameManager<Void, TestFrame> getRecycler() {
+        return (FrameManager<Void, TestFrame>) recycler;
     }
 
     @Override
@@ -70,10 +79,8 @@ public final class TestFrame extends AbstractFrame {
 
     @Override
     public void doFinally() {
-        if(countDown.decrementAndGet() == 0) {
-            if(trigger != null) {
-                trigger.countDown();
-            }
+        if(!recycle() && countDown != null) {
+            countDown.decrementAndGet();
         }
     }
 }

Euhedral-Engine/Euhedral-Execution

(cherry picked from commit 477a47caec88eb432a3952e5a1302248808ce5d7)

euhedral-core/src/main/java/euhedral/io/frames/ConsumerFrame.java
@@ -1,6 +1,6 @@
 package euhedral.io.frames;
 
-import euhedral.io.utils.MpscFrameRecycler;
+import euhedral.io.impl.FrameManager;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import lombok.Setter;
@@ -13,7 +13,7 @@ public class ConsumerFrame extends AbstractFrame {
     private Object payload;
 
     public ConsumerFrame(long idHash, Consumer<Object> consumer, AtomicBoolean killSwitch,
-            MpscFrameRecycler recycler) {
+            FrameManager<Object, ConsumerFrame> recycler) {
         super(idHash, recycler);
         this.consumer = consumer;
         this.killSwitch = killSwitch;

euhedral-core/src/main/java/euhedral/io/frames/FunctionFrame.java
@@ -1,6 +1,6 @@
 package euhedral.io.frames;
 
-import euhedral.io.utils.MpscFrameRecycler;
+import euhedral.io.impl.FrameManager;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -18,7 +18,7 @@ public class FunctionFrame extends AbstractFrame {
 
 
     public FunctionFrame(long idHash, Function<Object, Object> function, Consumer<Object> callback, AtomicBoolean killSwitch,
-            MpscFrameRecycler recycler) {
+            FrameManager<Object, FunctionFrame> recycler) {
         super(idHash, recycler);
         this.function = function;
         this.callback = callback;

euhedral-core/src/main/java/euhedral/io/frames/SequencedFrame.java
@@ -1,6 +1,7 @@
 package euhedral.io.frames;
 
-import euhedral.io.utils.MpscFrameRecycler;
+import euhedral.io.impl.FrameSequencer;
+import euhedral.io.impl.FrameManager;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import lombok.Getter;
@@ -34,7 +35,7 @@ public class SequencedFrame extends AbstractFrame {
 
     public SequencedFrame(long idHash, int sequenceNumber,
             Object payload, Function<Object, Object> function, AtomicBoolean killSwitch, FrameSequencer<Object> sequencer,
-            MpscFrameRecycler recycler) {
+            FrameManager<Object, SequencedFrame> recycler) {
         super(idHash, recycler);
         this.killSwitch = killSwitch;
         this.sequencer = sequencer;

euhedral-core/src/main/java/euhedral/io/impl/FrameFactory.java
@@ -1,0 +1,50 @@
+package euhedral.io.impl;
+
+import euhedral.io.frames.AbstractFrame;
+import euhedral.io.utils.KeyHasher;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class FrameFactory<T, F extends AbstractFrame> {
+    private final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
+
+    private final FrameCreate<T, F> frameGenerator;
+    private final FrameReplace<T, F> frameReplace;
+
+    private long seed = ThreadLocalRandom.current().nextLong();
+
+    public FrameFactory(FrameCreate<T, F> frameGenerator, FrameReplace<T, F> frameReplace) {
+        this.frameGenerator = frameGenerator;
+        this.frameReplace = frameReplace;
+    }
+
+    public F create(T data) {
+        F frame = frameGenerator.create(idHash, data);
+        if(frame.isOrdered()) {
+            frame.randomizeHash(idHash);
+        } else {
+            frame.randomizeHash(seed++);
+        }
+        return frame;
+    }
+
+    public void replace(T data, F frame) {
+        frame.reset();
+
+        frameReplace.replace(data, frame);
+        if(frame.isOrdered()) {
+            frame.randomizeHash(idHash);
+        } else {
+            frame.randomizeHash(seed++);
+        }
+    }
+
+    @FunctionalInterface
+    public interface FrameCreate<T, F extends AbstractFrame> {
+        F create(long idHash, T data);
+    }
+
+    @FunctionalInterface
+    public interface FrameReplace<T, F extends AbstractFrame> {
+        void replace(T data, F oldFrame);
+    }
+}

euhedral-core/src/main/java/euhedral/io/impl/FrameSequencer.java
@@ -1,7 +1,7 @@
-package euhedral.io.frames;
+package euhedral.io.impl;
 
+import euhedral.io.frames.SequencedFrame;
 import euhedral.io.utils.KeyHasher;
-import euhedral.io.utils.MpscFrameRecycler;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.longs.LongHeapPriorityQueue;
 import java.util.concurrent.ThreadLocalRandom;
@@ -15,7 +15,7 @@ import reactor.core.scheduler.Schedulers;
 
 public class FrameSequencer<R> {
 
-    private static final SequencedFrame SIGNAL_FRAME = new SequencedFrame( -1, -1, null, null,
+    private static final SequencedFrame SIGNAL_FRAME = new SequencedFrame(-1, -1, null, null,
             null, null, null);
 
     private final long ingestPassword;
@@ -36,68 +36,12 @@ public class FrameSequencer<R> {
         Flux.merge(ingest.asFlux())
                 .publishOn(Schedulers.boundedElastic())
                 .subscribe(frame -> {
-                    if(frame.getSequenceNumber() != -1) {
+                    if (frame.getSequenceNumber() != -1) {
                         frameData.put(frame.getSequenceNumber(), frame);
                         sequenceHeap.enqueue(frame.getSequenceNumber());
                     }
                     drainInternal();
                 });
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> Flux<AbstractFrame> map(Flux<T> flux, Function<T, R> function) {
-        final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
-        final AtomicBoolean killSwitch = new AtomicBoolean(false);
-
-        final long[] seed = new long[]{KeyHasher.combine(ThreadLocalRandom.current().nextLong(), ThreadLocalRandom.current().nextLong())};
-
-        final MpscFrameRecycler recycler = new MpscFrameRecycler(32_768, ingestPassword);
-
-        final int[] idx = new int[]{-1, 0};
-        final SequencedFrame[] buffer = new SequencedFrame[1024];
-
-        return flux.map(obj -> {
-            if (idx[0] < 0) {
-                idx[0] = recycler.batchPoll(buffer, ingestPassword);
-            }
-
-            SequencedFrame sequencedFrame;
-            if (--idx[0] >= 0) {
-                sequencedFrame = buffer[idx[0]];
-                sequencedFrame.replace(idx[1]++, obj);
-            } else {
-                sequencedFrame = new SequencedFrame(idHash, idx[1]++, obj,
-                        (Function<Object, Object>) function,
-                        killSwitch, (FrameSequencer<Object>) this, recycler);
-            }
-            sequencedFrame.setOrdered(false);
-            sequencedFrame.randomizeHash(seed[0]++);
-            registerFrame(sequencedFrame);
-
-            return (AbstractFrame) sequencedFrame;
-        }).doFinally(sig -> {
-            killSwitch.set(true);
-            sequenceHeap.clear();
-            frameData.clear();
-            ingest.tryEmitComplete();
-            output.tryEmitComplete();
-            recycler.close();
-        });
-    }
-
-    public Flux<R> output() {
-        return output.asFlux();
-    }
-
-    public void notifyComplete(long password) {
-        if(password == sequencePassword) {
-            ingest.tryEmitNext(SIGNAL_FRAME);
-        }
-    }
-
-    private void registerFrame(SequencedFrame frame) {
-        frame.setSequencerPassword(sequencePassword);
-        ingest.tryEmitNext(frame);
     }
 
     @SuppressWarnings("unchecked")
@@ -127,6 +71,52 @@ public class FrameSequencer<R> {
             } else {
                 break;
             }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> Flux<SequencedFrame> map(Flux<T> flux, Function<T, R> function) {
+        final AtomicBoolean killSwitch = new AtomicBoolean(false);
+
+        final int[] idx = new int[]{0};
+
+        FrameManager<Object, SequencedFrame> recycler = new FrameManager<>(32_768, ingestPassword);
+        recycler.setFactory(new FrameFactory<>((idHash, data) -> {
+            SequencedFrame frame = new SequencedFrame(idHash, idx[0]++, data,
+                    (Function<Object, Object>) function,
+                    killSwitch, (FrameSequencer<Object>) this, recycler);
+            frame.setOrdered(false);
+            registerFrame(frame);
+            return frame;
+        }, (data, oldFrame) -> {
+            oldFrame.replace(idx[0]++, data);
+            oldFrame.setOrdered(false);
+            registerFrame(oldFrame);
+        }));
+
+        return flux.map(obj -> recycler.generate(obj, ingestPassword))
+                .doFinally(sig -> {
+                    killSwitch.set(true);
+                    sequenceHeap.clear();
+                    frameData.clear();
+                    ingest.tryEmitComplete();
+                    output.tryEmitComplete();
+                    recycler.close();
+                });
+    }
+
+    private void registerFrame(SequencedFrame frame) {
+        frame.setSequencerPassword(sequencePassword);
+        ingest.tryEmitNext(frame);
+    }
+
+    public Flux<R> output() {
+        return output.asFlux();
+    }
+
+    public void notifyComplete(long password) {
+        if (password == sequencePassword) {
+            ingest.tryEmitNext(SIGNAL_FRAME);
         }
     }
 }

euhedral-core/src/test/java/euhedral/io/test_utils/TestFrame.java
@@ -4,48 +4,57 @@ import euhedral.io.control_plane.RoutingPolicy;
 import euhedral.io.frames.AbstractFrame;
 import euhedral.io.resource_monitoring.NumaMapper;
 import euhedral.io.utils.KeyHasher;
-import java.util.concurrent.CountDownLatch;
+import euhedral.io.impl.FrameManager;
 import java.util.concurrent.ThreadLocalRandom;
 import org.jctools.util.PaddedAtomicLong;
 
 public final class TestFrame extends AbstractFrame {
+    public static final long PASSWORD = 123;
 
     public long startNs;
     public PaddedAtomicLong countDown;
-    public CountDownLatch trigger;
     public boolean ordered;
 
-    public TestFrame(long idHash) {
-        this(idHash, true);
+    public TestFrame(long idHash, FrameManager<Void, TestFrame> recycler) {
+        this(idHash, true, recycler);
     }
 
-    public TestFrame(long idHash, boolean ordered) {
-        super(idHash, null);
+    public TestFrame(long idHash, boolean ordered, FrameManager<Void, TestFrame> recycler) {
+        super(idHash, recycler);
         this.ordered = ordered;
         this.setOrigin(NumaMapper.locateMe());
     }
 
     public static TestFrame[] generateParallel(int count) {
+        FrameManager<Void, TestFrame> recycler = new FrameManager<>(count, PASSWORD);
+
         final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
         TestFrame[] frames = new TestFrame[count];
         for (int i = 0; i < count; i++) {
-            frames[i] = new TestFrame(idHash, false);
+            frames[i] = new TestFrame(idHash, false, recycler);
             frames[i].randomizeHash(KeyHasher.getHash("seed-" + i));
         }
         return frames;
     }
 
     public static TestFrame[] generateOrdered(int count) {
+        FrameManager<Void, TestFrame> recycler = new FrameManager<>(count, PASSWORD);
+
         final long idHash = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
         final long orderedSeed = KeyHasher.mix(ThreadLocalRandom.current().nextLong());
 
         TestFrame[] frames = new TestFrame[count];
         for(int i = 0; i < count; i++) {
-            frames[i] = new TestFrame(idHash, true);
+            frames[i] = new TestFrame(idHash, true, recycler);
             frames[i].randomizeHash(orderedSeed);
             frames[i].setRoutingPolicy(RoutingPolicy.CORE_LOCAL);
         }
         return frames;
+    }
+
+    @SuppressWarnings("unchecked")
+    public FrameManager<Void, TestFrame> getRecycler() {
+        return (FrameManager<Void, TestFrame>) recycler;
     }
 
     @Override
@@ -70,10 +79,8 @@ public final class TestFrame extends AbstractFrame {
 
     @Override
     public void doFinally() {
-        if(countDown.decrementAndGet() == 0) {
-            if(trigger != null) {
-                trigger.countDown();
-            }
+        if(!recycle() && countDown != null) {
+            countDown.decrementAndGet();
         }
     }
 }