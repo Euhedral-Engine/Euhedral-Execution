10a555272ce0c54ee62a1833eefa9be796194762
Brandon <euhedral.io@gmail.com>
2026-05-19 22:32:35 -0400

    Added LatencyBenchmark

--- benchmarks/src/main/java/euhedral/benchmarks/BenchRunner.java
+++ benchmarks/src/main/java/euhedral/benchmarks/BenchRunner.java
@@ -1,52 +1,62 @@
 package euhedral.benchmarks;
 
+import euhedral.benchmarks.core_benchmarks.LatencyBenchmark;
 import euhedral.benchmarks.core_benchmarks.MandelbrotBenchmark;
+import euhedral.benchmarks.core_benchmarks.ThroughputBenchmark;
+import java.util.ArrayList;
+import java.util.List;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class BenchRunner {
-
+    private static final List<String> FLAGS = List.of("-XX:+UseThreadPriorities",
+            "--enable-native-access=ALL-UNNAMED",
+            "--add-exports", "java.base/jdk.internal.platform=ALL-UNNAMED",
+            "--add-exports", "java.base/jdk.internal.vm.annotation=ALL-UNNAMED",
+            "--add-opens", "java.base/java.util=ALL-UNNAMED",
+            "-Dorg.slf4j.simpleLogger.defaultLogLevel=error");
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
-            System.out.println("Please specify a benchmark to run. Options: mandelbrot");
+            System.out.println(
+                    "Please specify a benchmark to run. Options: [core-latency, core-throughput, mandelbrot]");
             return;
         }
 
         String name = args[0].toLowerCase();
+
         Class<?> benchmark;
-        if (name.equals("mandelbrot")) {
-            benchmark = MandelbrotBenchmark.class;
-        } else {
-            System.out.println("Unknown benchmark: " + name);
-            System.out.println("Please specify a benchmark to run. Options: mandelbrot");
-            return;
-        }
+        List<String> flags = new ArrayList<>(FLAGS);
+        switch (name) {
+            case "mandelbrot" -> {
+                benchmark = MandelbrotBenchmark.class;
+                String degree = System.getProperty("degree", "2");
+                flags.add("-Ddegree=" + degree);
+                flags.add("-DoutputDir=" + System.getProperty("outputDir", "results"));
 
-        String degree = System.getProperty("degree", "5"); // default to 5 if not passed
-        String outputFileName = System.getProperty("outputDir", "mandelbrot-D" + degree);
-        String outputDir = System.getProperty("outputDir", "");
-
-        if(!outputFileName.endsWith(".png")) {
-            outputFileName += ".png";
+                String fileName = System.getProperty("outputFile", "mandelbrot-D" + degree);
+                if (!fileName.endsWith(".png")) {
+                    fileName += ".png";
+                }
+                flags.add("-DoutputFile=" + fileName);
+            }
+            case "core-throughput" -> benchmark = ThroughputBenchmark.class;
+            case "core-latency" -> benchmark = LatencyBenchmark.class;
+            default -> {
+                System.out.println("Unknown benchmark: " + name);
+                System.out.println(
+                        "Please specify a benchmark to run. Options: [core-latency, core-throughput, mandelbrot]");
+                return;
+            }
         }
 
         ChainedOptionsBuilder opt =
                 new OptionsBuilder().include(benchmark.getSimpleName())
-                        .warmupIterations(0)
                         .forks(1)
-                        .measurementIterations(1)
                         .addProfiler("gc")
-                        .addProfiler("perf",
-                                "events=cycles,instructions,cache-misses,L2-loads,L2-load-misses")
-                        .jvmArgs("-XX:+RestrictContended", "-XX:+UseThreadPriorities",
-                                "--enable-native-access=ALL-UNNAMED",
-                                "--add-exports", "java.base/jdk.internal.platform=ALL-UNNAMED",
-                                "--add-exports", "java.base/jdk.internal.vm.annotation=ALL-UNNAMED",
-                                "--add-opens", "java.base/java.util=ALL-UNNAMED",
-                                "-Ddegree=" + degree, "-DoutputFile=" + outputFileName,
-                                "-DoutputDir=" + outputDir,
-                                "-Dorg.slf4j.simpleLogger.defaultLogLevel=error");
+//                        .addProfiler("perf",
+//                                "events=cycles,instructions,cache-misses,L2-loads,L2-load-misses")
+                        .jvmArgsAppend(flags.toArray(new String[0]));
         new Runner(opt.build()).run();
     }
 }
