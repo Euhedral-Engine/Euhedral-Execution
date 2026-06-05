6fa94097e2d3b44a6ae59191ea01f27b924cbcdd
Brandon <euhedral.io@gmail.com>
2026-06-04 22:14:52 -0400

    Added the ChunkAllocator

--- euhedral-data-structures/src/main/java/euhedral/experimental/UnboundedSpscQueue.java
+++ euhedral-data-structures/src/main/java/euhedral/experimental/UnboundedSpscQueue.java
@@ -6,8 +6,21 @@ import java.util.function.Consumer;
 @SuppressWarnings({"unchecked", "unused"})
 public final class UnboundedSpscQueue<T> extends BaseConcurrentQueue {
 
+    private final ChunkAllocator allocator;
+
     public UnboundedSpscQueue(int chunkSize) {
         super(chunkSize);
+        this.allocator = null;
+    }
+
+    public UnboundedSpscQueue(int chunkSize, int maxPooledChunks) {
+        super(chunkSize);
+
+        if(maxPooledChunks > 0) {
+            this.allocator = new ChunkAllocator(maxPooledChunks, maxPooledChunks);
+        } else {
+            this.allocator = null;
+        }
     }
 
     public boolean offer(T obj) {
@@ -38,6 +51,21 @@ public final class UnboundedSpscQueue<T> extends BaseConcurrentQueue {
     public void clear() {
         while (scDrain(QueueUtils.NO_OP, Long.MAX_VALUE) > 0) {
             Thread.onSpinWait();
+        }
+    }
+
+    @Override
+    protected Object[] allocateChunk(int chunkSize) {
+        if(allocator == null) {
+            return new Object[chunkSize];
+        }
+        return allocator.allocate(chunkSize);
+    }
+
+    @Override
+    protected void freeChunk(Object[] chunk) {
+        if(allocator != null) {
+            allocator.free(chunk);
         }
     }
 }
