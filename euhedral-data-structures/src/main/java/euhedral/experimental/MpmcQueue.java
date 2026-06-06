70f442a3ce4f61ffb8f37af7922807f9281777d9
Brandon <euhedral.io@gmail.com>
2026-06-06 19:26:52 -0400

    Fixed drain bug with multi-consumer queues

---
+++ euhedral-data-structures/src/main/java/euhedral/experimental/MpmcQueue.java
@@ -0,0 +1,129 @@
+package euhedral.experimental;
+
+import euhedral.queues.common.QueueUtils;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+@SuppressWarnings({"unchecked", "unused"})
+public final class MpmcQueue<T> extends BaseConcurrentQueue.MultiConsumer<T> {
+
+    private final ChunkAllocator allocator;
+    private final long maxConsumeBatch;
+
+    public MpmcQueue(int chunkSize) {
+        this(chunkSize, 0, Long.MAX_VALUE);
+    }
+
+    public MpmcQueue(int chunkSize, int maxPooledChunks) {
+        this(chunkSize, maxPooledChunks, Long.MAX_VALUE);
+    }
+
+    public MpmcQueue(int chunkSize, int maxPooledChunks, long maxConsumeBatch) {
+        super(chunkSize);
+        if (maxConsumeBatch <= 0) {
+            throw new IllegalArgumentException("maxConsumeBatch must be positive");
+        }
+
+        if (maxPooledChunks > 0) {
+            this.allocator = new ChunkAllocator(maxPooledChunks, maxPooledChunks);
+        } else {
+            this.allocator = null;
+        }
+        this.maxConsumeBatch = maxConsumeBatch;
+    }
+
+    @Override
+    public boolean offer(T obj) {
+        mpOffer(obj);
+        return true;
+    }
+
+    @Override
+    public T peek() {
+        if (!acquireMcLock(this)) {
+            return null;
+        }
+        try {
+            return (T) scPeek();
+        } finally {
+            releaseMcLock(this);
+        }
+    }
+
+    @Override
+    public T poll() {
+        if (!acquireMcLock(this)) {
+            return null;
+        }
+        try {
+            return (T) scPoll();
+        } finally {
+            releaseMcLock(this);
+        }
+    }
+
+    @Override
+    public void fill(T[] objs) {
+        mpFill(objs);
+    }
+
+    @Override
+    public void fill(Iterable<T> objs) {
+        mpFill((Iterable<Object>) objs);
+    }
+
+    @Override
+    public long drain(Consumer<T> consumer, long limit) {
+        Objects.requireNonNull(consumer);
+        if (limit <= 0) {
+            return 0;
+        }
+
+        long total = 0;
+        while (total < limit) {
+            if (!acquireMcLock(this)) {
+                return total;
+            }
+            try {
+                long batch = Math.min(limit - total, this.maxConsumeBatch);
+                long temp = scDrain((Consumer<Object>) consumer, batch);
+                if(temp == 0) {
+                    break;
+                }
+                total += temp;
+            } finally {
+                releaseMcLock(this);
+            }
+        }
+        return total;
+    }
+
+    @Override
+    public void clear() {
+        while (!acquireMcLock(this)) {
+            Thread.onSpinWait();
+        }
+        try {
+            while (scDrain(QueueUtils.NO_OP, Long.MAX_VALUE) > 0) {
+                Thread.onSpinWait();
+            }
+        } finally {
+            releaseMcLock(this);
+        }
+    }
+
+    @Override
+    protected Object[] allocateChunk(int chunkSize) {
+        if (this.allocator == null) {
+            return new Object[chunkSize];
+        }
+        return this.allocator.allocate(chunkSize);
+    }
+
+    @Override
+    protected void freeChunk(Object[] chunk) {
+        if (this.allocator != null) {
+            this.allocator.free(chunk);
+        }
+    }
+}
--- euhedral-data-structures/src/main/java/euhedral/experimental/SpscQueue.java
+++ euhedral-data-structures/src/main/java/euhedral/experimental/SpscQueue.java
@@ -4,15 +4,15 @@ import euhedral.queues.common.QueueUtils;
 import java.util.function.Consumer;
 
 @SuppressWarnings({"unchecked", "unused"})
-public final class UnboundedSpscQueue<T> extends BaseConcurrentQueue {
+public final class SpscQueue<T> extends BaseConcurrentQueue<T> {
 
     private final ChunkAllocator allocator;
 
-    public UnboundedSpscQueue(int chunkSize) {
+    public SpscQueue(int chunkSize) {
         this(chunkSize, 0);
     }
 
-    public UnboundedSpscQueue(int chunkSize, int maxPooledChunks) {
+    public SpscQueue(int chunkSize, int maxPooledChunks) {
         super(chunkSize);
 
         if (maxPooledChunks > 0) {
@@ -22,31 +22,38 @@ public final class UnboundedSpscQueue<T> extends BaseConcurrentQueue {
         }
     }
 
+    @Override
     public boolean offer(T obj) {
         spOffer(obj);
         return true;
     }
 
+    @Override
     public T peek() {
         return (T) scPeek();
     }
 
+    @Override
     public T poll() {
         return (T) scPoll();
     }
 
+    @Override
     public void fill(T[] objs) {
         spFill(objs);
     }
 
+    @Override
     public void fill(Iterable<T> objs) {
         spFill((Iterable<Object>) objs);
     }
 
+    @Override
     public long drain(Consumer<T> consumer, long limit) {
         return scDrain((Consumer<Object>) consumer, limit);
     }
 
+    @Override
     public void clear() {
         while (scDrain(QueueUtils.NO_OP, Long.MAX_VALUE) > 0) {
             Thread.onSpinWait();
