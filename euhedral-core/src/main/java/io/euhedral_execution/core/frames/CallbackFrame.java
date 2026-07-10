9150b74f18169ea2e273e22efe9b1896e3f98f7f
Brandon <euhedral.io@gmail.com>
2026-07-10 18:19:37 -0400

    Created the ChainFrame, CallbackFrame, LatticeHotReceiver, and EuhedralSink

---
+++ euhedral-core/src/main/java/io/euhedral_execution/core/frames/CallbackFrame.java
@@ -0,0 +1,59 @@
+package io.euhedral_execution.core.frames;
+
+import io.euhedral_execution.core.generics.FramePusher;
+import io.euhedral_execution.core.impl.FrameManager;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import lombok.Getter;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+public final class CallbackFrame<T, R> extends ChainFrame<CallbackFrame<T, R>>{
+
+    private final Function<T, R> function;
+
+    @Getter
+    private T payload;
+    @Getter
+    private R retVal;
+
+    public CallbackFrame(long idHash,
+            T payload,
+            @NonNull Function<T, R> function,
+            @NonNull FramePusher<ChainFrame<CallbackFrame<T, R>>> responseReceiver) {
+        super(idHash, responseReceiver);
+        Objects.requireNonNull(function);
+        this.function = function;
+        this.payload = payload;
+    }
+
+    public CallbackFrame(long idHash,
+            T payload,
+            @NonNull Function<T, R> function,
+            @NonNull FramePusher<ChainFrame<CallbackFrame<T, R>>> responseReceiver,
+            @Nullable FrameManager<T, CallbackFrame<T, R>> recycler,
+            @Nullable AtomicBoolean killSwitch) {
+        super(idHash, responseReceiver, recycler, killSwitch);
+        Objects.requireNonNull(function);
+        this.function = function;
+        this.payload = payload;
+    }
+
+    @Override
+    public void execute() {
+        this.retVal = this.function.apply(this.payload);
+        giveToReceiver(this);
+    }
+
+    /// Intentionally empty. recycle() must be called manually
+    @Override
+    public void doFinally() {
+
+    }
+
+    public void replace(T payload) {
+        this.payload = payload;
+        this.retVal = null;
+    }
+}
