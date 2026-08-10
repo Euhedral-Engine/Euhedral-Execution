0471b4a991c0e18ac7394c7f48bd46764db4ab13
brandon <euhedral.io@gmail.com>
2026-08-10 09:59:01 -0400

    Added spotless

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.palantir.java-format:com.palantir.java-format.gradle.plugin:2.73.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:8.9.0")
     implementation("org.junit.jupiter:junit-jupiter-api:5.10.5")
 }
